e8d163a46bec4bf2228549d965c11f3ec3b99dd9
stephany0318 <stephany.bermudez.mt@correo.usa.edu.co>
2021-10-27 23:31:28 -0500

    first commit

---
+++ src/main/java/retomoto/Reto3moto/RepositorioMotorbike.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package retomoto.Reto3moto;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Repository
+public class RepositorioMotorbike {
+    
+    @Autowired
+    private InterfaceMotorbike crud;
+
+    public List<Motorbike> getAll(){
+        return (List<Motorbike>) crud.findAll();
+    }
+
+    public Optional<Motorbike> getBike(int id){
+        return crud.findById(id);
+    }
+
+    public Motorbike save(Motorbike motorbike){
+        return crud.save(motorbike);
+    }
+    public void delete(Motorbike motorbike){
+        crud.delete(motorbike);
+    }
+
+    Optional<Motorbike> getMotorbike(int motorbikeId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
